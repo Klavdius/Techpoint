6224289151acd549eda1e7346f86f6be61ea66ea
DESKTOP-913FP8Q\ObLom <klavdiuc@gmail.com>
2024-02-02 20:50:51 +0300

    work programm but not accept test

--- DataCompression/Compression.go
+++ DataCompression/Compression.go
@@ -10,14 +10,13 @@ import (
 
 func main() {
 	var (
-		amountRecords         int
-		lengInputString       int
-		previousElem          int
-		rowSing               string
-		inputString           string
-		lineNotHaveZeroNumber bool
-		flag                  bool
+		amountRecords   int
+		lengInputString int
+		inputString     string
 	)
+
+	finalLine := []int{}
+
 	fmt.Scanln(&amountRecords)
 	for i := 0; i < amountRecords; i++ {
 		fmt.Scanln(&lengInputString)
@@ -27,43 +26,50 @@ func main() {
 		inputRawData := strings.Split(inputString, " ")
 		inputData := ConvertSliceToInt(inputRawData)
 
-		firstElem := inputData[0]
-		previousElem = inputData[0]
-		lineNotHaveZeroNumber = true
-		line := []int{}
-		for inner := 1; inner < lengInputString; inner++ {
-			if lineNotHaveZeroNumber == true {
-				if inputData[inner] == 0 {
-					flag = true
-				}
-			} else {
-				if inputData[inner] == 0 {
-					rowSing = FindRowSing(line)
-					fmt.Print(strconv.Itoa(firstElem) + " " + rowSing + strconv.Itoa(len(line)) + " ")
-					firstElem = inputData[inner]
-					line = nil
-					lineNotHaveZeroNumber = true
-					flag = false
-				}
+		for inner := 0; inner < lengInputString; inner++ {
 
+			lengTestLine := FindLengRowInInput(inputData)
+
+			testOnZero, positionZero := TestOnDoubleZeroInLine(inputData[:lengTestLine])
+
+			for testOnZero > 1 {
+				lengTestLine = CuteDoubleZeroInLine(inputData[:positionZero+1])
+				testOnZero, positionZero = TestOnDoubleZeroInLine(inputData[:lengTestLine])
 			}
 
-			if previousElem-inputData[inner] == 1 || previousElem-inputData[inner] == -1 {
-				line = append(line, inputData[inner])
-				if flag {
-					lineNotHaveZeroNumber = false
-				}
-			} else {
-				rowSing = FindRowSing(line)
-				fmt.Print(strconv.Itoa(firstElem) + " " + rowSing + strconv.Itoa(len(line)) + " ")
-				firstElem = inputData[inner]
-				line = nil
-				lineNotHaveZeroNumber = true
-				flag = false
-			}
+			test2 := testOnOneRowSing(inputData[:lengTestLine])
 
-			previousElem = inputData[inner]
+			for !test2 {
+				lengTestLine--
+				test2 = testOnOneRowSing(inputData[:lengTestLine])
+			}
+			line := inputData[:lengTestLine]
+			sing := FindRowSing(line)
+
+			number := 0
+			switch sing {
+			case "":
+				number = 0
+			case "-":
+				number = (len(line) - 1) * -1
+			case "+":
+				number = (len(line) - 1)
+			}
+			finalLine = append(finalLine, line[0])
+			finalLine = append(finalLine, number)
+			inputData = inputData[len(line):]
+
+			if len(inputData) == 0 {
+				break
+			}
 		}
+		fmt.Println(len(finalLine))
+
+		for _, v := range finalLine {
+			fmt.Print(strconv.Itoa(v) + " ")
+		}
+		fmt.Println()
+		finalLine = nil
 	}
 }
 
@@ -75,6 +81,68 @@ func ConvertSliceToInt(input []string) []int {
 		intSlice = append(intSlice, elem)
 	}
 	return intSlice
+}
+
+func FindLengRowInInput(line []int) int {
+	firstElem := line[0]
+	nextElem := firstElem
+	sum := 1
+	for i, v := range line {
+		if i != 0 {
+			if nextElem-v == 1 || nextElem-v == -1 {
+				sum++
+				nextElem = v
+			} else {
+				break
+			}
+		}
+	}
+
+	return sum
+}
+
+func TestOnDoubleZeroInLine(line []int) (int, int) {
+	result := 0
+	positionZero := 0
+	for i, v := range line {
+		if v == 0 {
+			result++
+			positionZero = i
+		}
+	}
+
+	return result, positionZero
+}
+
+func CuteDoubleZeroInLine(line []int) int {
+	line = line[:len(line)-1]
+	return len(line)
+}
+
+func testOnOneRowSing(line []int) bool {
+	result := true
+	if len(line) != 1 {
+		sing := line[0] - line[1]
+		if sing > 0 {
+			for i, v := range line {
+				if i != 0 {
+					if v >= line[0] {
+						result = false
+					}
+				}
+			}
+		} else {
+			for i, v := range line {
+				if i != 0 {
+					if v <= line[0] {
+						result = false
+					}
+				}
+			}
+		}
+	}
+
+	return result
 }
 
 func FindRowSing(line []int) string {
